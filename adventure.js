669c30da069b4d84795ee7e48f20a8bfb757a73f
Valentijn <99057635@mydavinci.nl>
2019-12-07 17:37:21 +0100

    Verhaallein bedacht voor de game. Strat pagina klaar. lvl1 bedacht

--- adventure.js
+++ adventure.js
@@ -3,6 +3,7 @@ var image = document.getElementById('image');
 var button1 = document.getElementById('button1');
 var button2 = document.getElementById('button2');
 var button3 = document.getElementById('button3');
+var pagetitle = document.get
 var description = document.getElementById('description');
 var title = document.getElementById('title');
 
@@ -10,8 +11,8 @@ var beer = false;
 
 
 function startgame(){
-  title.innerHTML = "Het drink spel!";
-  description.innerHTML = "Je gaat met je vrienden de club in en je moet een aantal<br>beslissingen nemen terwijl je hebt gedronken!";
+  title.innerHTML = "Survival weekend";
+  description.innerHTML = "Je gaat met je vrienden  een Survival weekend houden in de Belgische Ardennen!<br>Hier ga je dan ook gevaarlijke activiteiten ondernemen!";
   button1.innerHTML = "Start Game";
   button2.style.display = 'none';
   button3.style.display = 'none';
@@ -19,11 +20,29 @@ function startgame(){
   button1.setAttribute("onclick", 'level1()');
 }
 function level1(){
-  description.innerHTML = "";
+  description.innerHTML = "De eerste Survival activiteit die je gaat ondernemen is klimmen!<br>Kies uit één van de volgende acties!<br> Keuze 1: Je gaat klimmen zonder  veiligheidsapparatuur!<br> Keuze 2: Je gaat klimmen en abseilen met behulp van een begeleider!<br>Keuze 3: Je gaat klimmen zonder zonder schoenen!";
+  image.setAttribute("src","images/klimmen.jpg");
   button2.style.display = "inline-block";
   button3.style.display = "inline-block";
-  button3.innerHTML = "keuze";
+  button1.innerHTML = "Keuze 1";
+  button2.innerHTML = "Keuze 2";
+  button3.innerHTML = "Keuze 3";
+  button1.setAttribute("onclick", 'doodlvl1()');
 }
+
+
+//NIET WERKEN CODE START NIET
+// function doodlvl1(){
+//   title.innerHTML = :"Dood!";
+//   description.innerHTML = "Je valt dood omdat je je grip verliest en je geen veiligheidsapparatuur aan hebt!";
+//   image.setAttribute("src","images/vallen.jpg");
+//   button2.style.display = 'none';
+//   button3.style.display = 'none';
+//   button1.innerHTML = "Restart Game";
+//   button1.setAttribute("onclick", 'startgame()');
+// }
+
+
 
 startgame();
 
---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "bs2qCA0cCfQ?t=2563",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 80,
+
+}
